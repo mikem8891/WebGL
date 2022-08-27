f3ee650f2215a00eb23d9943f40262282ea97eb7
Mike Maley <michael.r.maley@gmail.com>
2022-08-27 14:46:36 -0400

    gamma correction

--- Demo_6/fshader.glsl
+++ Demo_6/fshader.glsl
@@ -10,17 +10,12 @@ struct DirectionalLight{
 uniform sampler2D sampler;
 uniform vec3 ambLightInt;
 uniform DirectionalLight sun;
-//uniform vec3 sunlightInt;
-//uniform vec3 sunlightDir;
 
 varying vec2 fragTexCoord;
 varying vec3 fragNormal;
 
 void main(){
   
-//  vec3 ambLightInt = vec3( 0.1,  0.1,  0.1);
-//  vec3 sunlightInt = vec3( 1.5,  1.5,  1.5);
-//  vec3 sunlightDir = vec3(-3.0,  1.0, -4.0);
   vec3 normSunDir  = normalize(sun.direction);
   vec3 surfaceNorm = normalize(fragNormal);
   
@@ -29,6 +24,7 @@ void main(){
   vec3 lightInt = ambLightInt + 
     sun.color * max(dot(normSunDir, surfaceNorm), 0.0);
   
-  gl_FragColor = texel * vec4(lightInt, 1.0);
-//  gl_FragColor = vec4(surfaceNorm, 1.0); // for debugging
+  vec4 linearColor = texel * vec4(lightInt, 1.0);
+  gl_FragColor = sqrt(linearColor);  // pseudo gamma correction
+//  gl_FragColor = vec4(surfaceNorm, 1.0); // display normals for debugging
 }
