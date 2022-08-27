db9e4e42d6a325a8e98e30853bb2ca5cef20c0c7
Mike Maley <michael.r.maley@gmail.com>
2022-08-27 14:21:52 -0400

    demo 6

---
+++ Demo_6/vshader.glsl
@@ -0,0 +1,20 @@
+// vertex shader
+
+precision mediump float;
+
+uniform mat4 mWorld;
+uniform mat4 mView;
+uniform mat4 mProj;
+
+attribute vec3 vertPosition;
+attribute vec3 vertNormal;
+attribute vec2 vertTexCoord;
+
+varying vec2 fragTexCoord;
+varying vec3 fragNormal;
+
+void main(){
+  gl_Position  = mProj * mView * mWorld * vec4(vertPosition, 1.0);
+  fragNormal   = (mWorld * vec4(vertNormal, 0.0)).xyz;
+  fragTexCoord = vertTexCoord;
+}
