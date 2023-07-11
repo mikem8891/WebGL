de752810ee473ba35a7d7ab983b1b703ebd04888
Mike Maley <michael.r.maley@gmail.com>
2023-07-10 22:11:00 -0400

    Update mandel.fs.glsl

--- Mandelbrot_double/mandel.fs.glsl
+++ Mandelbrot_double/mandel.fs.glsl
@@ -16,6 +16,41 @@ uniform float rngI;
 uniform float avgI;
 uniform float avgIe;
 
+vec2 dbit(vec2 a){
+  float cx = a.x + a.y;
+  float cy = (a.x - cx) + a.y;
+  return vec2(cx, cy);
+}
+
+vec2 dadd(vec2 a, vec2 b){
+  float cx = a.x + b.x; // Most significant bits
+  float cy = (a.x + (b.x - cx)) + (b.x + (a.x - cx)) + a.y + b.y; //Accounting for any error in cx and adding the least significant bits
+  return dbit(vec2(cx, cy));
+}
+
+vec2 dmul(vec2 a, vec2 b){
+  float bit12 = 4096.0;     // Constant for shifting a value 12 bits
+  float a12x = bit12 * a.x; 
+  float b12x = bit12 * b.x;
+  float axx = (a.x + a12x) - a12x; // Truncates the 12 least sig bits in a.x
+  float bxx = (b.x + b12x) - b12x; // Truncates the 12 least sig bits in b.x
+  float axy = a.x - axx; // The 12 least sig bits in a.x
+  float bxy = b.x - bxx; // The 12 least sig bits in b.x
+  float cx = a.x * b.x;  // Most significant bits of the result
+  float cy = ((axx * bxx - cx) + axx * bxy + axy * bxx) + axy * bxy + a.x * b.y + a.y * b.x;
+  return vec2(cx, cy);
+}
+
+vec2 dsq(vec2 a){
+  float bit12 = 4096.0;     // Constant for shifting a value 12 bits
+  float a12x = bit12 * a.x; 
+  float axx = (a.x + a12x) - a12x; // Truncates the 12 least sig bits in a.x
+  float axy = a.x - axx; // The 12 least sig bits in a.x
+  float cx = a.x * a.x;  // Most significant bits of the result
+  float cy = ((axx * axx - cx) + 2.0 * axx * axy) + axy * axy + 2.0 * a.x * a.y;
+  return vec2(cx, cy);
+}
+
 void main(){
   
   // Transform pixel space to Mandelbrot set space
@@ -29,11 +64,13 @@ void main(){
     (gl_FragCoord.x / viewportDim.x - 0.5) * rngR,
     (gl_FragCoord.y / viewportDim.y - 0.5) * rngI
   );
-  vec2 c  =  vec2(avgR, avgI) + dc;
-  vec2 ce = (vec2(avgR, avgI) - c) + dc + vec2(avgRe, avgIe);
+  vec2 ch =  vec2(avgR, avgI) + dc;
+  vec2 cl = (vec2(avgR, avgI) - ch) + dc + vec2(avgRe, avgIe);
+  vec2 cx = vec2(ch.x, cl.x);
+  vec2 cy = vec2(ch.y, cl.y);
 
-  vec2 z  = c;
-  vec2 ze = ce;
+  vec2 x  = cx;
+  vec2 y  = cy;
   float iter = 0.0;
   const float maxIters = 2000.0;
   const int maxii = 2000;
@@ -43,24 +80,15 @@ void main(){
   // while |z(2000)| < 2
   
   for (int i = 0; i < maxii; i++){
-    float te = 2.0 * (ze.x * z.y + z.x * ze.y + ze.x * ze.y) + ce.y;
-    ze.x = 2.0 * (z.x * ze.x - z.y * ze.y) + ze.x * ze.x - ze.y * ze.y + ce.x;
-    ze.y = te;
 
-    float t = 2.0 * z.x * z.y + c.y;    // temp var for z.y(i+1)
-    z.x = z.x * z.x - z.y * z.y + c.x;  // z.x(i+1)
-    z.y = t;
+    vec2 t = dadd(2.0 * dmul(x, y), cy); // temp var for z.y(i+1)
+    x = dadd(dadd(dsq(x), -dsq(y)), cx); // z.x(i+1) = z.x^2  - z.y^y  + c.x
+    y = t;                               // z.y(i+1) = 2.0 * z.x * z.y + c.y
     
-    float zx = z.x + ze.x;
-    ze.x = ze.x - (zx - z.x);
-    z.x = zx;
-    float zy = z.y + ze.y;
-    ze.y = ze.y - (zy - z.y);
-    z.y = zy;
-    if (zx * zx + zy * zy > 4.0){
+    vec2 mag = dadd(dsq(x), dsq(y));
+    if (mag.x > 4.0){
       break;
     }
-    
     iter += 1.0;
   }
   
